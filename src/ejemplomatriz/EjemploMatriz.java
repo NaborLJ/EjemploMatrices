7bcdcb0df91fb9e5b3128910b9b16c0fce541c04
nlopezjimenez <nlopezjimenez@atanasoff23.danielcastelao.org>
2016-01-21 12:43:44 +0100

    Inacabado

---
+++ src/ejemplomatriz/EjemploMatriz.java
@@ -0,0 +1,19 @@
+
+package ejemplomatriz;
+
+/**
+ *
+ * @author nlopezjimenez
+ */
+public class EjemploMatriz {
+
+    
+    public static void main(String[] args) {
+        MetodosMatriz obx = new MetodosMatriz();
+        obx.cargarMatriz();
+        obx.amosarMatriz();
+        obx.amosarMediaMatriz();
+        obx.calcularMediaModulos();
+    }
+    
+}
